7d38c5210ac9f70b1699f4fc268d02fcff70ed7e
mbouchenoire <gitbouchenoire@gmail.com>
2017-07-16 12:16:44 +0200

    Allow the user the choose a day and a place

--- discloud/command_handler.py
+++ discloud/command_handler.py
@@ -9,12 +9,40 @@ class CommandHandler(object):
         self.weather_service = weather_service
 
     async def handle(self, command):
-        if not command.content.startswith("!meteo") and not command.content.startswith("!weather"):
+        INVALID_FORMAT_MESSAGE = "the weather command format must be '!weather <offset>@<place>' (e.g. !weather +1@Nantes)"
+
+        content = command.content
+
+        if not content.startswith("!meteo") and not content.startswith("!weather"):
             return
 
-        tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
-        weather = self.weather_service.get_forecast("Nantes,fr", tomorrow)
-        embed = self.__create_embed_weather__(weather, "Nantes", tomorrow)
+        content = content.replace("!meteo", "").replace("!weather", "")
+
+        if "@" not in content:
+            return await self.discord_client.send_message(command.channel, INVALID_FORMAT_MESSAGE)
+
+        parts = content.split("@")
+
+        if len(parts) != 2:
+            return await self.discord_client.send_message(command.channel, INVALID_FORMAT_MESSAGE)
+
+        offset_str = parts[0]
+        place = parts[1]
+
+        offset = 0
+
+        if offset_str is not None and offset_str.strip() != "":
+            offset = int(offset_str)
+
+            if offset < 0:
+                return await self.discord_client.send_message(command.channel, "the forecast daily offset must be >= 0")
+
+        if place is None or place == "":
+            return await self.discord_client.send_message(command.channel, "the place is mandatory (@<place>)")
+
+        day = datetime.datetime.today() + datetime.timedelta(days=offset)
+        weather = self.weather_service.get_forecast(place, day)
+        embed = self.__create_embed_weather__(weather, day, place)
 
         await self.discord_client.send_message(command.channel, embed=embed)
 
@@ -30,17 +58,27 @@ class CommandHandler(object):
 
         return TEMPLATE.format(SIZE, google_icon_code)
 
-    def __create_embed_weather__(self, weather, place, date) -> discord.Embed:
+    def __create_embed_weather__(self, weather, date, place) -> discord.Embed:
         detailed_status = weather._detailed_status.title()
         weather_code = weather._weather_code
         humidity = weather.get_humidity()
-        temperature_min = weather.get_temperature("celsius")["min"]
-        temperature_max = weather.get_temperature("celsius")["max"]
+        temperature = weather.get_temperature("celsius")
+
+        temperature_min = None
+        temperature_max = None
+        
+        if "temp_min" in temperature: # when manipulating today's weather (observation)
+            temperature_min = temperature["temp_min"]
+            temperature_max = temperature["temp_max"]
+        elif "min" in temperature: # when manipulating forecast
+            temperature_min = temperature["min"]
+            temperature_max = temperature["max"]
+
         wind_speed = int(weather.get_wind()["speed"] * 3.6)
         icon_url = CommandHandler.__weather_code_to_google_icon_url__(weather_code)
 
         embed = discord.Embed(colour=0x00FF00)
-        embed.title = "Weather forecast @ {}: tomorrow ({})".format(place, date.strftime("%A %d %B"))
+        embed.title = "{} @ {}".format(date.strftime("%A %d %B"), place.title())
         embed.description = detailed_status
         embed.add_field(name="High temp.", value=str(int(temperature_max)) + "°C")
         embed.add_field(name="Low temp.", value=str(int(temperature_min)) + "°C")
--- discloud/weather.py
+++ discloud/weather.py
@@ -1,10 +1,16 @@
+import datetime
 import pyowm
 
 class WeatherService(object):
     def __init__(self, pyowm_api_key: str):
         self.owm = pyowm.OWM(pyowm_api_key)
 
-    def get_forecast(self, place: str, when):
-        forecast = self.owm.daily_forecast(place)
-        return forecast.get_weather_at(when)
+    def get_forecast(self, place: str, when: datetime.datetime):
+        is_today = when.date() == datetime.date.today()
+
+        if is_today:
+            return self.owm.weather_at_place(place).get_weather()
+        else:
+            forecast = self.owm.daily_forecast(place)
+            return forecast.get_weather_at(when)
     
